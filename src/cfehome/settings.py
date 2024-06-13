a3d293e4ef094773b14703acc0f0119332ad7fcc
Dimeji Rufai <rfdimeji@gmail.com>
2024-06-13 16:22:48 +0100

    add neon database integration and dotenv functionality

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from decouple import config
+from os import getenv
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +24,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@rkldw+*0!jl#vtkjaphlrl-^ai0n)69a4t*15&o9u@r!ktbe2'
+#SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = getenv('DJANGO_SECRET_KEY')
+#SECRET_KEY = "django-insecure-@rkldw+*0!jl#vtkjaphlrl-^ai0n)69a4t*15&o9u@r!ktbe2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = str(getenv("DJANGO_DEBUG")).lower() == "true"
+#DEBUG = os.environ.get('DJANGO_DEBUG')
+#DEBUG = config("DJANGO_DEBUG", cast=bool)
+print("DEBUG", DEBUG, type(DEBUG))
 ALLOWED_HOSTS = [
-    ".railway.app"          # hhtps://saas.prod.railway.app
+    ".railway.app"          # https://saas.prod.railway.app
 ]
 if DEBUG:
     ALLOWED_HOSTS += [
@@ -90,6 +98,24 @@ DATABASES = {
     }
 }
 
+# Replace the DATABASES section of your settings.py with this
+DATABASE_URL = str(getenv("PGHOST"))
+print("\nDATABASE_URL", type(DATABASE_URL))
+
+if DATABASE_URL is not None:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv('PGDATABASE'),
+        'USER': getenv('PGUSER'),
+        'PASSWORD': getenv('PGPASSWORD'),
+        'HOST': getenv('PGHOST'),
+        'PORT': getenv('PGPORT', 5432),
+        'OPTIONS': {
+        'sslmode': 'require',
+        },
+    }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
